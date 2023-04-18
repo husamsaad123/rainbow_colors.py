8dcfca1b2baa5bc2a34c0f75b5c244da3cbfa781
husamsaad123 <125978708+husamsaad123@users.noreply.github.com>
2023-04-18 19:09:36 +0300

    Create rainbow_colors.py

---
+++ rainbow_colors.py
@@ -0,0 +1,7 @@
+# Create a list of rainbow colors
+colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+
+# Print the rainbow colors
+print("The colors of the rainbow are:")
+for color in colors:
+    print(color)
